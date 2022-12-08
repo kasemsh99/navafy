abdd131770f437fa36c00e054d774481e9ac0f36
Mujtabamirzaeii <116367088+Mujtabamirzaeii@users.noreply.github.com>
2022-12-08 18:26:54 +0330

    followed_users_list

---
+++ Api/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('register/', views.user_register),
+    path('login/', views.user_login),
+	path('user/', views.user_data),
+	path('profile/edit/', views.profile_edit),
+
+	path('artist/', views.artist_data),
+	path('artist/edit/', views.artist_edit),
+	path('user/followed/list/', views.followed_users_list),
+	path('favorite/create/', views.favorite_create),
+	path('post/create/', views.post_create),
+
+]
