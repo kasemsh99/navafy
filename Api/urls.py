d848f8a4a9d7373d414df2062d2f0410c95886a7
Mujtabamirzaeii <116367088+Mujtabamirzaeii@users.noreply.github.com>
2022-12-08 19:04:28 +0330

    user_unfollow

--- Api/urls.py
+++ Api/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
 
 	path('artist/', views.artist_data),
 	path('artist/edit/', views.artist_edit),
+	path('user/unfollow/', views.user_unfollow),
 	path('user/followed/list/', views.followed_users_list),
 	path('favorite/create/', views.favorite_create),
 	path('post/create/', views.post_create),
